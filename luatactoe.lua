6300efb75c709cd669f7208a9a61348ab155233c
RygelD <43830270+RygelD@users.noreply.github.com>
2020-12-02 17:04:32 -0500

    Update luatactoe.lua
    started work on evaluation function
    fixed bug where 3,5,9 was victory instead of 3,6,9

--- luatactoe.lua
+++ luatactoe.lua
@@ -2,15 +2,12 @@
 --2020 Rygel Dagenais
 --made while working on a lua chess program
 
-board = {0,0,0,0,0,0,0,0,0}
-xmove = true
 
-function reset()
-  board = {0,0,0,0,0,0,0,0,0}
-  xmove = true
+function new()
+  return {0,0,0,0,0,0,0,0,0,xmove=true}
 end
 
-function genmoves()
+function genmoves(board)
   local valid = {}
   for i = 1,9 do
     if board[i] == 0 then
@@ -20,13 +17,13 @@ function genmoves()
   return valid
 end
 
-function setmove(n)
-  if xmove == true then
+function setmove(board,n)
+  if board['xmove'] == true then
     board[n] = 1
   else
     board[n] = 2
   end
-  xmove = not xmove
+  board['xmove'] = not board['xmove']
 end
 
 function no_value(tab, val)
@@ -39,24 +36,25 @@ function no_value(tab, val)
 end
 
 
-function getmove()
-  print(makeboard())
+function getmove(board)
+  print(makeboard(board))
   local msg = ''
-  if xmove then
+  if board['xmove'] then
     msg = '\nX to move: '
   else
     msg = '\nO to move: '
   end
-  local v = genmoves()
+  local v = genmoves(board)
   local a = 0
   while no_value(v,a) do
     io.write(msg)
     a = io.read('*n')
+    io.write('\n\n=====\n\n')
   end
   return a
 end
 
-function makeboard()
+function makeboard(board)
   local b = {}
   for i = 1,9 do
     if board[i] == 0 then
@@ -70,16 +68,16 @@ function makeboard()
   return string.format('%s|%s|%s\n-+-+-\n%s|%s|%s\n-+-+-\n%s|%s|%s',b[1],b[2],b[3],b[4],b[5],b[6],b[7],b[8],b[9])
 end
 
-function checkwin()
+function checkwin(board)
   local all = {
-  ck(1,2,3),
-  ck(4,5,6),
-  ck(7,8,9),
-  ck(1,4,7),
-  ck(2,5,8),
-  ck(3,5,9),
-  ck(1,5,9),
-  ck(3,5,7)}
+  ck(board,1,2,3),
+  ck(board,4,5,6),
+  ck(board,7,8,9),
+  ck(board,1,4,7),
+  ck(board,2,5,8),
+  ck(board,3,6,9),
+  ck(board,1,5,9),
+  ck(board,3,5,7)}
   for i = 1,8 do
     if all[i] ~= 0 then
       return all[i]
@@ -88,7 +86,7 @@ function checkwin()
   return 0
 end
 
-function ck(a,b,c)
+function ck(board,a,b,c)
   if board[a] == board[b] then
     if board[b] == board[c] then
       return board[c]
@@ -98,27 +96,42 @@ function ck(a,b,c)
 end
 
 function playboth()
-  reset()
+  board = new()
   local v = false
   for i = 1,9 do
-    setmove(getmove())
-    a = checkwin()
+    setmove(board,getmove(board))
+    a = checkwin(board)
     if a == 1 then
-      print(makeboard())
+      print(makeboard(board))
       print('\nX wins!')
       v = true
       break
     elseif a == 2 then
-      print(makeboard())
+      print(makeboard(board))
       print('\nO wins!')
       v = true
       break
     end
   end
   if v == false then
-    print(makeboard())
+    print(makeboard(board))
     print('\nIt\'s a tie...')
   end
 end
+
+function evaluate(board,player,depth)
+  local z = checkwin(board) 
+  if player == z then
+    return {1000,nil}
+  elseif z ~= 0 then
+    return {-1000,nil}
+  end
+  local a = getmove(board)
+  local p = {}
+  for i = 1,#a do
+    local a = false
+  end
+end
+
 
 playboth()
